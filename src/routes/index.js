4a6e0414468f4c6dfa34b6b03a99094160886b4f
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-03-10 16:01:29 -0500

    Added eras & spinoffs routes to index, Swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use('/', require('./swagger'));
 router.use('/movies', require('./movies'))
 router.use('/characters', require('./characters'))
-// router.use('/eras', require('./eras'))
-// router.use('/spinoff', require('./spinoff'))
+router.use('/eras', require('./eras'))
+router.use('/spinoffs', require('./spinoff'))
 
 module.exports = router;
