185007ac7ebc775202100746d45a263cb9e52b50
mucacran <el.mucacran.rasta@hotmail.com>
2023-03-28 05:56:29 -0500

    actualizaciones de sergio

--- __tests__/puttt.test.js
+++ __tests__/puttt.test.js
@@ -25,7 +25,7 @@ describe("llamada a conectarse a la base de datos", ()=>{
     it('should insert a doc into collection', async () => {
         const movies = db.collection('movies');
         const mockUser = {
-            id: 'some-user-id',
+            _id: 'some-user-id',
             title: 'Old Movie Title',
             promoImage: 'Old Movie Image',
             year: '2021',
@@ -36,8 +36,37 @@ describe("llamada a conectarse a la base de datos", ()=>{
             category: 'Old Movie Category',
         };
         await movies.insertOne(mockUser);
-        const insertedUser = await movies.findOne({ id: 'some-user-id' });
+        const insertedUser = await movies.findOne({ _id: 'some-user-id' });
         expect(insertedUser).toEqual(mockUser);
     });
-}
-);
+
+    it('se actualiza en la base de datos PUT', async () => {
+        const movies = db.collection('movies');
+        //movieId = insertedMovie.insertedId.toString();
+        const mockMovieUpdate = {
+            title: 'Otro titulo',
+            promoImage: 'Otra cosa',
+            year: '2023',
+            era: 'Otra Contemporary',
+            length: '120',
+            trailerLink: 'Old Movie Trailer',
+            trivia: 'Old Movie Trivia',
+            category: 'Old Movie Category',
+        };
+        //const updateMovie = await movies.replaceOne({ _id: 'some-user-id' },mockMovieUpdate);
+        //expect(updateMovie.result.modifiedCount).toEqual(1);
+
+        // Busca el documento antes de actualizarlo
+  const movieToUpdate = await movies.findOne({ _id: 'some-user-id' });
+  // Verifica que el documento existe antes de intentar actualizarlo
+  if (movieToUpdate) {
+    const updateMovie = await movies.replaceOne(movieToUpdate, mockMovieUpdate, { upsert: false });
+    // Verifica que el documento se actualizó correctamente
+    //expect(updateMovie.result.matchedCount).toEqual(1);
+    expect(updateMovie).toEqual(mockMovieUpdate);
+  } else {
+    throw new Error('El documento no existe en la base de datos');
+  }
+});
+
+});
